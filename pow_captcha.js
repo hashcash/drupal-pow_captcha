f3589d7ef8c36fe11ef9531bd4d2639e9ca55fdc
Pavel Karoukin <pavel@karoukin.us>
2014-06-07 19:03:39 -0500

    based on version 0.2.x of hashcash.io

--- pow_captcha.js
+++ pow_captcha.js
@@ -1,7 +1,5 @@
-/* global Drupal, POW, jQuery */
+/* global Drupal, jQuery */
 (function($) {
-  var serverUrl;
-  var working = false;
   var badBrowser = false;
 
   if (navigator.appName.indexOf("Internet Explorer") != -1) {
@@ -12,84 +10,28 @@
 
   Drupal.behaviors.pow_captcha = {
     attach: function(c) {
-      serverUrl = Drupal.settings.pow_captcha.server_url;
       $("form input[name='pow_captcha']", c).once('pow_captcha', attach_pow_captcha);
     }
   };
 
   function attach_pow_captcha() {
-    var form = $(this).parents('form').first();
-
-    form.addClass("pow-captcha-attached");
-
-    $(".form-actions input[type='submit']", form).each(function() {
-      var b = $(this);
-      if (! b.attr('disabled')) {
-        b.data('pow-captcha-disabled', true);
-        b.attr('disabled', 'disabled').addClass("form-button-disabled");
-      }
-    });
+    var $form = $(this).parents('form').first();
 
     if (badBrowser) {
-        $(".pow-captcha-ie-warning", form).show();
-        $(".pow-captcha-progress-wrapper", form).hide();
-        $('input[name="pow_captcha_enable_buttons"]', form).parents('.form-item').first().hide();
+        $(".pow-captcha-ie-warning", $form).show();
         return;
     }
 
-    // Attach work call whenever user interact with form
-    $("input", form).one('focus', function() { work(form); });
-    $('input[name="pow_captcha_enable_buttons"]', form).one('click', function() { work(form); });
-  }
+    $form.addClass('pow-captcha-attached');
 
-  function work(form) {
-    if (working) { return; }
-    working = true;
+    var $button = $form.find("input[type=submit]").eq(0);
 
-    // Hide checkbox and show progress bar
-    $('input[name="pow_captcha_enable_buttons"]', form).parents('.form-item').first().hide();
-    $(".pow-captcha-progress-wrapper").show();
-    var pb = new Drupal.progressBar('pow-captcha-progress-bar');
-    $(pb.element).appendTo( form.find('.pow-captcha-progress') );
-    pb.setProgress("0", Drupal.t("Generating Proof-of-Work..."));
-
-    var form_build_id = $("input[name='form_build_id']", form).val();
-
-    var pow = new POW(
-      Drupal.settings.pow_captcha.key,
-      serverUrl,
-      Drupal.settings.pow_captcha.worker_url
-    );
-
-    pow.setLimit( Drupal.settings.pow_captcha.complexity );
-
-    pow.setDoneCallback(function() {
-      // Hide captcha
-      setTimeout(function() {
-        $(".pow-captcha-progress-wrapper", form).hide();
-        $("fieldset.captcha", form).slideUp('fast');
-      }, 500);
-
-      // Enable submit buttons
-      $(".form-actions input[type='submit']", form).each(function() {
-        var b = $(this);
-        if (b.data('pow-captcha-disabled')) {
-          b.removeAttr("disabled").removeClass("form-button-disabled");
-        }
-      });
+    $button.hashcash({
+        autoId: false,
+        hashcashInputName: "form_build_id",
+        key: Drupal.settings.pow_captcha.key,
+        complexity: Drupal.settings.pow_captcha.complexity
     });
-
-    pow.setWorkProgressCallback(function(doneCount) {
-      var percentage = Math.floor( doneCount / Drupal.settings.pow_captcha.complexity * 100 );
-      if (percentage >= 100) {
-        pb.setProgress(100, Drupal.t("Done."));
-      }
-      else {
-        pb.setProgress(percentage, Drupal.t("Generating Proof-of-Work..."));
-      }
-    });
-
-    pow.work(form_build_id);
   }
 
 })(jQuery);
